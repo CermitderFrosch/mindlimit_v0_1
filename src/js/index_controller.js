929441f5b3e11c6d9f542231edf0e7c18dda13f5
Christian Ullrich <christian@codefragments.org>
2020-07-05 18:32:34 +0200

    Backend test added

--- src/js/index_controller.js
+++ src/js/index_controller.js
@@ -8,6 +8,14 @@ class IndexController {
     //Check content elements and request their html content from the backend
     loadContent(){
         this.log("Starting to load content for index.html");
+
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function(){
+            console.log(this.responseXML);
+        }
+        xhr.open("GET", "../src/php/backend_test.php");
+        xhr.responseType = "document";
+        xhr.send();
     }
 
     log(output){
